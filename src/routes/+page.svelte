8903c8aa00afc153ce01cfdaac4cb3df46025123
Putra Budiman <cristminix@gmail.com>
2026-01-14 20:39:42 +0700

    setup tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,6 @@
   documentation
 </p>
 
-<div className="flex min-h-svh flex-col items-center justify-center">
+<!-- <div className="flex min-h-svh flex-col items-center justify-center">
   <Button>Click me</Button>
-</div>
+</div> -->
